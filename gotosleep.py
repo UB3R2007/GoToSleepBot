5b85bbdacdade202f329374c5a2eb6cc5d041f32
UB3R <77548855+umairkazi2007@users.noreply.github.com>
2023-04-02 22:28:26 -0400

    Added WIP files from first day

---
+++ gotosleep.py
@@ -0,0 +1,41 @@
+# GoToSleep.py
+
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(f'{client.user.name} has connected to Discord!')
+    
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    if client.user.id != message.author.id:
+        if message.content == "test":
+            await message.reply("response")
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+client.run(TOKEN)
